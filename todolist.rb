b10ebeb4c6d902cf18aad01c4c80f723a8e472ba
Duncan McFarland <duncanmcfarland@outlook.com>
2016-06-28 01:52:55 -0700

    Add the rest of the setup code from the lesson

--- todolist.rb
+++ todolist.rb
@@ -6,8 +6,18 @@ class TodoList
 		@items = Array.new
 	end
 	
+	def add_item(new_item)
+		item = Item.new(new_item)
+		@items.push(item)
+	end
+	
 end
 
 class Item
-    # methods and stuff go here
+	attr_reader :description, :completed_status
+	
+	def initialize(item_description)
+		@description = item_description
+		@completed_status = false
+	end
 end
